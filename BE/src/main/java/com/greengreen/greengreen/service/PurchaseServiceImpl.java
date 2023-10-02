13a0d96e43af3858b969139664840ac0fdbfdbdd
iihye <floral_berry@naver.com>
2023-10-02 14:32:18 +0900

    Feat : Purchase 기능 함수 구현 231002 1432

---
+++ BE/src/main/java/com/greengreen/greengreen/service/PurchaseServiceImpl.java
@@ -0,0 +1,43 @@
+package com.greengreen.greengreen.service;
+
+import com.greengreen.greengreen.dto.request.PurchaseBuyReqDto;
+import com.greengreen.greengreen.entity.Product;
+import com.greengreen.greengreen.entity.Purchase;
+import com.greengreen.greengreen.entity.User;
+import com.greengreen.greengreen.repository.ProductRepository;
+import com.greengreen.greengreen.repository.PurchaseRepository;
+import com.greengreen.greengreen.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PurchaseServiceImpl implements PurchaseService{
+    private final PurchaseRepository purchaseRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+
+    // 우편번호 검색
+    
+
+    // 구매
+    @Override
+    public void buyPurchase(PurchaseBuyReqDto purchaseBuyReqDto) {
+        User user = userRepository.findByUserId(purchaseBuyReqDto.getUserId())
+                .orElseThrow(()->new RuntimeException("유효하지 않은 user입니다."));
+
+        Product product = productRepository.findByProductId(purchaseBuyReqDto.getProductId())
+                .orElseThrow(()->new RuntimeException("유효하지 않은 product입니다."));
+
+        Purchase purchase = Purchase.builder()
+                .purchaseName(purchaseBuyReqDto.getPurchaseName())
+                .purchaseAddress(purchaseBuyReqDto.getPurchaseAddress())
+                .purchaseNumber(purchaseBuyReqDto.getPurchaseNumber())
+                .purchasePhoneNumber(purchaseBuyReqDto.getPurchasePhoneNumber())
+                .user(user)
+                .product(product)
+                .build();
+    }
+}
