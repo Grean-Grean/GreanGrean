ca827d6e05768d447be0c9bce486b6151e63b479
unknown <20191792@edu.hanbat.ac.kr>
2023-10-02 16:03:24 +0900

    Feat : axios 백엔드 연결 231002 1603

--- fe/src/components/user/SignUpData.js
+++ fe/src/components/user/SignUpData.js
@@ -3,65 +3,40 @@ import axios from "axios";
 import classes from "./SignUpData.module.css";
 
 function SignUpData(props) {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmationPassword, setConfirmationPassword] = useState("");
-  const [verificationCode, setVerificationCode] = useState("");
+  const [formData, setFormData] = useState({
+    userName: "",
+    userNickName: "",
+    userEmail: "",
+    userPassword: "",
+    confirmPassword: "",
+  });
+
   const [emailExists, setEmailExists] = useState(false);
   const [passwordMismatch, setPasswordMismatch] = useState(false);
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleNicknameChange = (e) => {
-    setNickname(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    setEmailExists(false); // Reset emailExists state when email changes
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    setPasswordMismatch(false); // Reset passwordMismatch state when password changes
-  };
-
-  const handleConfirmationPasswordChange = (e) => {
-    setConfirmationPassword(e.target.value);
-    setPasswordMismatch(false); // Reset passwordMismatch state when confirmation password changes
-  };
-
-  const handleVerificationCodeChange = (e) => {
-    setVerificationCode(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    if (name === "userEmail") {
+      setEmailExists(false); // Reset emailExists state when email changes
+    } else if (name === "userPassword" || name === "confirmPassword") {
+      setPasswordMismatch(false); // Reset passwordMismatch state when password changes
+    }
   };
 
   const handleSignUp = () => {
     // Perform validation here before making the axios request
-    if (password !== confirmationPassword) {
+    if (formData.userPassword !== formData.confirmPassword) {
       setPasswordMismatch(true);
       return;
     }
 
-    axios.defaults.baseURL="http://localhost:8080/user/regist"
-
     // Send data to the server using axios POST request
     axios
-      .post("/", {
-        name: name,
-        nickname: nickname,
-        email: email,
-        password: password,
-        verificationCode: verificationCode,
-        headers: {
-          'Access-Control-Allow-Origin': '*', // 실제 운영 환경에서는 '*' 대신 신뢰할 수 있는 도메인을 지정해야 합니다.
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-          'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-        },
-      })
+      .post("http://172.30.1.23:8080/user/regist", formData)
       .then((response) => {
         // Handle the response from the server here
         console.log(response.data);
@@ -71,49 +46,87 @@ function SignUpData(props) {
         console.error(error);
       });
   };
+
   return (
     <>
       <p>
-        <label htmlFor="name">이름</label>
-        <input type="text" id="name" onChange={props.onNameChange}></input>
+        <label htmlFor="userName">이름</label>
+        <input
+          type="text"
+          id="userName"
+          name="userName"
+          value={formData.userName}
+          onChange={handleInputChange}
+        />
       </p>
 
       <p>
-        <label htmlFor="body">닉네임</label>
-        <input type="text" onChange={props.onNicknameChange}></input>
+        <label htmlFor="userNickName">닉네임</label>
+        <input
+          type="text"
+          id="userNickName"
+          name="userNickName"
+          value={formData.userNickName}
+          onChange={handleInputChange}
+        />
         <button>중복확인</button>
       </p>
       <p>
-        <label htmlFor="body">이메일</label>
-        <input type="email" onChange={props.onEmailChange}></input>
+        <label htmlFor="userEmail">이메일</label>
+        <input
+          type="email"
+          id="userEmail"
+          name="userEmail"
+          value={formData.userEmail}
+          onChange={handleInputChange}
+        />
         <button>인증하기</button>
         <br />
-        <label htmlFor="body" className={classes.alert}>
-          이미 등록된 이메일입니다.
-        </label>
+        {emailExists && (
+          <label htmlFor="userEmail" className={classes.alert}>
+            이미 등록된 이메일입니다.
+          </label>
+        )}
       </p>
 
       <p>
-        <label htmlFor="body">비밀번호</label>
+        <label htmlFor="userPassword">비밀번호</label>
         <input
           type="password"
+          id="userPassword"
+          name="userPassword"
           placeholder="영어, 숫자, 특수문자를 포함한 8자리 이상"
-          onChange={props.onPwChange}
-        ></input>
+          value={formData.userPassword}
+          onChange={handleInputChange}
+        />
       </p>
 
       <p>
-        <label htmlFor="body">비밀번호 확인</label>
-        <input type="password"></input>
+        <label htmlFor="confirmPassword">비밀번호 확인</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleInputChange}
+        />
         <br />
-        <label htmlFor="body" className={classes.alert}>
-          비밀번호가 일치하지 않습니다.
-        </label>
+        {passwordMismatch && (
+          <label htmlFor="confirmPassword" className={classes.alert}>
+            비밀번호가 일치하지 않습니다.
+          </label>
+        )}
       </p>
 
       <p>
-        <label htmlFor="body">인증번호</label>
-        <input type="text"></input>
+        <label htmlFor="verificationCode">인증번호</label>
+        <input
+          type="text"
+          id="verificationCode"
+          name="verificationCode"
+          value={formData.verificationCode}
+          onChange={handleInputChange}
+        />
         <button>확인</button>
       </p>
       <button onClick={handleSignUp}>가입하기</button>
