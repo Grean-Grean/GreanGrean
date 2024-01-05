0f2187e98d52c45fc8ec7d74682c3dc49defe6c8
holizon9 <holizon9@naver.com>
2024-01-06 00:40:45 +0900

    fix : UserRepository 수정 240106 0040

--- BE/src/main/java/com/greengreen/greengreen/repository/UserRepository.java
+++ BE/src/main/java/com/greengreen/greengreen/repository/UserRepository.java
@@ -16,4 +16,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(Long userId);
     Optional<User> findByUserNickName(String userNickName);
+
 }
