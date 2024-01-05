f9d2c06ea5bb073a98ad2c8d48aac38820dd65a4
iihye <floral_berry@naver.com>
2024-01-06 03:35:29 +0900

    Feat : 리뷰조회 기능 구현 240106 0335

--- BE/src/main/java/com/greengreen/greengreen/repository/ReviewRepository.java
+++ BE/src/main/java/com/greengreen/greengreen/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.greengreen.greengreen.repository;
 import com.greengreen.greengreen.entity.Product;
 import com.greengreen.greengreen.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Optional<Review> findByReviewId(Long reviewId);
     Optional<Review> deleteByReviewId(Long reviewId);
+
+    @Query("select r from Review r where r.product.productId = :productId")
+    List<Review> findAllByProductId(Long productId);
 }
